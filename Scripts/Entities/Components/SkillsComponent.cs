a666f8f981fafda0b9f43a8912d658a11ab8d2d0
LittleCrowRevi <snowrevy@gmail.com>
2023-10-10 18:10:43 +0200

    add skill level

--- Scripts/Entities/Components/SkillsComponent.cs
+++ Scripts/Entities/Components/SkillsComponent.cs
@@ -32,7 +32,15 @@ public abstract class Skill
     public abstract string Name { get; set; }
     public abstract string Description { get; set; }
     public abstract SkillMode SkillMode { get; set; }
+    public int SkillLevel { get; set; } = 0;
+    public abstract List<int> DamageScaleLevels { get; set; }
     
+    public void SetSkillLevel(int newSkillLevel)
+    {
+        // TODO: Proper Handling of max level, response back to UI that level is at max
+        var dmgScaleLevels = DamageScaleLevels.Count;
+        SkillLevel = Math.Clamp(newSkillLevel + 1, 0, dmgScaleLevels);
+    }
 }    
 public enum SkillMode
      {
--- Scripts/Skills/ActiveSkills.cs
+++ Scripts/Skills/ActiveSkills.cs
@@ -1,13 +1,22 @@
-﻿using ThronesEra.Scripts.Entities.Components;
+﻿using System.Collections.Generic;
+using ThronesEra.Scripts.Entities.Components;
 
 namespace ThronesEra.Scripts.Skills;
 
-public class ActiveSkills
+public static class ActiveSkills
 {
     public class Fireball : Skill
     {
         public override string Name { get; set;  } = "Fireball";
         public override string Description { get; set; } = "Fires a ball of fire at the target";
         public override SkillMode SkillMode { get; set; } = SkillMode.Active;
+
+        public override List<int> DamageScaleLevels { get; set; } = new()
+        {
+            100,
+            110,
+            120,
+            150
+        };
     }
 }
